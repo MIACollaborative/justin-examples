b92d81d156a6af36fd6dad3322234fece92ef9e1
Pei-Yao Hung <peiyaoh@gmail.com>
2025-07-17 16:48:35 -0400

    Update test_email.ts

--- break-away/src/test_script/test_email.ts
+++ break-away/src/test_script/test_email.ts
@@ -1,7 +1,7 @@
 import { EmailUtility } from "../lib/email-utility";
 import { config } from "dotenv";
 config();  
-EmailUtility.sendEmail("BreakAway Notification", process.env.VERIFIED_SENDER_EMAIL as string, [{name: "Test Recipient", address: process.env.TEST_RECIPIENT_EMAIL as string}], "Break Away", "Break Away Test").then((result) => {
+EmailUtility.sendEmail("BreakAway Notification", process.env.VERIFIED_SENDER_EMAIL as string, [{name: "Test Recipient", address: process.env.TEST_RECIPIENT_EMAIL_1 as string}], "Break Away", "Break Away Test").then((result) => {
   console.log("Email sent successfully:", result);
 }).catch((error) => {
   console.error("Error sending email:", error);
