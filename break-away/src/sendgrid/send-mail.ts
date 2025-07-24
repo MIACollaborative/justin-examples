74e4d4274fc5da335307799fb1a814908e07255f
Jose Rubio <jadrubio@umich.edu>
2025-07-24 14:06:39 -0400

    added in sendgrid

---
+++ break-away/src/sendgrid/send-mail.ts
@@ -0,0 +1,26 @@
+import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const sendGridSendEmail = async (to: string | string[], subject: string, text: string): Promise<any> => {
+  if (!process.env.SENDGRID_API_KEY) {
+    throw new Error("SENDGRID_API_KEY is not set");
+  }
+
+  if (!process.env.VERIFIED_SENDER_EMAIL) {
+    throw new Error("VERIFIED_SENDER_EMAIL is not set");
+  }
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to,
+    from: process.env.VERIFIED_SENDER_EMAIL,
+    subject,
+    text,
+  };
+
+  const result = await sgMail.send(msg);
+  return result;
+};
+
