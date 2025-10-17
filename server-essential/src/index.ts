8e82a36779036e3383e33d99b8bc00eac544d50b
Pei-Yao Hung <peiyaoh@gmail.com>
2025-10-17 14:49:58 -0400

    Add server-essential package with example guards and server
    Introduces the server-essential package, including package.json, example guard implementations, and a demonstration server script. The guards provide generic, permission, and token validation middleware for Express, and the server script shows how to configure and use these guards with endpoint registration and logging.

---
+++ server-essential/src/index.ts
@@ -0,0 +1,77 @@
+// This is the example server script that shows how to use just-in server
+// It is here for demonstration / testing purposes only
+// This will not be part of the actual just-in server package
+
+import { JAppServer, JAppServer2, DEFAULT_GUARDS, HTTPMethods } from 'src'; // eventually, @just-in/server (or core for Log, depends on future decision)
+import JustIn, { Log } from '@just-in/core';
+import { Logger } from 'src/server/justin-core'; // need type export from @just-in/core in the future?
+import { guardGeneric, requestTokenValidatorGuard } from './guards';
+
+// option 1: let the server create its own JustIn instance
+//const server = JAppServer();
+const server = JAppServer2();
+const port = process.env.PORT || 3001;
+
+// option 2: pass in your own JustIn instance
+/*
+const justIn = JustIn();
+
+// set your logging levels here, before passing into server
+justIn.setLoggingLevels({
+  dev: false,
+  info: true,
+  warn: true,
+  error: true
+});
+*/
+
+// optionally, assign your own logger
+// otherwise it will use the default console logger (Log) from just-in core
+const myLogger: Logger = {
+  info: (message?: any, ...optionalParams: any[]) => {
+    console.log(`[Example] ${message}`, ...optionalParams);
+  },
+  warn: (message?: any, ...optionalParams: any[]) => {
+    console.warn(`[Example] ${message}`, ...optionalParams);
+  },
+  error: (message?: any, ...optionalParams: any[]) => {
+    console.error(`[Example] ${message}`, ...optionalParams);
+  },
+  dev: (message?: any, ...optionalParams: any[]) => {
+    console.debug(`[Example] ${message}`, ...optionalParams);
+  },
+};
+server.configureLogger(myLogger);
+
+// override guards, leave the controller intact
+server.overrideDefaultEndpoint({path: '/api/users', method: HTTPMethods.GET, guards: [guardGeneric]});
+
+// add a new guard, and override existing controller
+server.overrideDefaultEndpoint(
+  {path: '/api/users/:userUniqueIdentifier', method: HTTPMethods.DELETE, guards: [...DEFAULT_GUARDS, guardGeneric], 
+    controller: (req, res) => { res.send('User deleted (not actually) - overridden'); }}
+);
+
+// Issue a warning, as /api/notexist is not handled by default configuration
+server.overrideDefaultEndpoint({path: '/api/notexist', method: HTTPMethods.GET, guards: []});
+
+// add new endpoints
+// note that the did not use default guards
+server.registerEndpoint({path: '/api/test/hello', method: HTTPMethods.GET, guards: [guardGeneric], controller: (req, res) => { res.send('Hello!'); }});
+server.registerEndpoint({path: '/api/test/hi', method: HTTPMethods.GET, guards: [guardGeneric], controller: (req, res) => { res.send('Hi!'); }});
+server.registerEndpoint({path: '/api/test/yo', method: HTTPMethods.GET, guards: [guardGeneric], controller: (req, res) => { res.send('Yo!'); }});
+server.registerEndpoint({path: '/api/test/token', method: HTTPMethods.GET, guards: [requestTokenValidatorGuard], controller: (req, res) => { res.send('Token!'); }});
+
+// This one should error out, because it is overlapping with default endpoint
+//server.registerEndpoint({path: '/api/users/:userUniqueIdentifier', method: HTTPMethods.DELETE, guards: [guardGeneric], controller: (req, res) => { res.send('User deleted (not actually)'); }});
+
+
+// start the server, which includes initializing justin and database connection
+server.start(port)
+.then(() => {
+  Log.info(`Server started`);
+})
+.catch((err: Error) => {
+  Log.error("Error starting server:", err);
+});
+
